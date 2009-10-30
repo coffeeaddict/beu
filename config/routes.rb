620594ce413b3d6a36ade16f28102fb05c0f8543
Hartog C. de Mik <hartog.de.mik@simplic.it>
2009-10-30 14:07:52 +0000

    wrong routing

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ ActionController::Routing::Routes.draw do |map|
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   # map.resources :beu
 
-  map.connect 'beu/:id', :controller => "beu", :action => "index"
-  
+ 
   # Sample resource route with options:
   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
 
